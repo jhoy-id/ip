af8639da9f630665443deb9897194016d7546dc7
jhoy-id <89542179+jhoy-id@users.noreply.github.com>
2021-12-29 18:22:32 +0700

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -9,6 +9,12 @@ wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/ghiwID/Script-V
 mkdir -p /home/vps/public_html
 wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/ghiwID/Script-VPN/master/vps.conf"
 /etc/init.d/nginx restart
-
 wget /home/vps/public_html https://raw.githubusercontent.com/jhoy-id/ip/main/liuuufeydevmain
 chmod +x /home/vps/public_html/liuuufeydevmain
+cd
+
+#akses
+cd /usr/bin
+wget -O /usr/bin/addip https://raw.githubusercontent.com/jhoy-id/ip/main/addip.sh && chmod +x /usr/bin/addip
+wget -O /usr/bin/delip https://raw.githubusercontent.com/jhoy-id/ip/main/delip.sh && chmod +x /usr/bin/delip
+wget -O /usr/bin/xp-ip https://raw.githubusercontent.com/jhoy-id/ip/main/xp-ip.sh && chmod +x /usr/bin/xp-ip
