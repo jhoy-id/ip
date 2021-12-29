2020efcb54d50a892caa8e4fce9fefde013bda37
jhoy-id <89542179+jhoy-id@users.noreply.github.com>
2021-12-30 06:49:00 +0700

    Update akses.sh

--- akses.sh
+++ akses.sh
@@ -5,9 +5,9 @@ apt -y install nginx
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/ghiwID/Script-VPN/master/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/jhoy-id/sc-noip/main/nginx.conf"
 mkdir -p /home/vps/public_html
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/ghiwID/Script-VPN/master/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/jhoy-id/sc-noip/main/vps.conf"
 /etc/init.d/nginx restart
 wget /home/vps/public_html https://raw.githubusercontent.com/jhoy-id/ip/main/liuuufeydevmain
 chmod +x /home/vps/public_html/liuuufeydevmain
