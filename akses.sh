c46838c2c4195fa004423bc136c4c5dad732323d
Liuuufey <89542179+liuuufey@users.noreply.github.com>
2022-02-17 17:37:39 +0700

    Update akses.sh

--- akses.sh
+++ akses.sh
@@ -5,20 +5,19 @@ apt -y install nginx
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/jhoy-id/sc-noip/main/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/liuuufey/sc-noip/main/nginx.conf"
 mkdir -p /home/vps/public_html
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/jhoy-id/sc-noip/main/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/liuuufey/sc-noip/main/vps.conf"
 /etc/init.d/nginx restart
-wget -O /home/vps/public_html "https://raw.githubusercontent.com/jhoy-id/ip/main/liuuufeydevmain"
+wget -O /home/vps/public_html "https://raw.githubusercontent.com/liuuufey/ip/main/liuuufeydevmain"
 chmod +x /home/vps/public_html/liuuufeydevmain
 cd
 
 #akses
 cd /usr/bin
-wget -O /usr/bin/addip https://raw.githubusercontent.com/jhoy-id/ip/main/addip.sh && chmod +x /usr/bin/addip
-wget -O /usr/bin/delip https://raw.githubusercontent.com/jhoy-id/ip/main/delip.sh && chmod +x /usr/bin/delip
-wget -O /usr/bin/xp-ip https://raw.githubusercontent.com/jhoy-id/ip/main/xp-ip.sh && chmod +x /usr/bin/xp-ip
+wget -O /usr/bin/addip https://raw.githubusercontent.com/liuuufey/ip/main/addip.sh && chmod +x /usr/bin/addip
+wget -O /usr/bin/delip https://raw.githubusercontent.com/liuuufey/ip/main/delip.sh && chmod +x /usr/bin/delip
+wget -O /usr/bin/xp-ip https://raw.githubusercontent.com/liuuufey/ip/main/xp-ip.sh && chmod +x /usr/bin/xp-ip
 cd
-rm -rf liuuufeydevmain
 rm -rf akses.sh
 
